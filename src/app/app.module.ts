914177d1cfc261193e17fcd25d0fc9d181defc5a
gojohill <gojohill@gmail.com>
2019-03-27 21:29:13 -0400

    fixed development from merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 
-import { AlertComponent } from './_components';
+import { AlertRetaskComponent } from './_components';
 //import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from '@app/pages/home';
 import { LoginComponent } from '@app/pages/login';
@@ -16,7 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { CalendarComponent } from '@app/pages/calendar/calendar.component'
+import { CalendarComponent } from '@app/pages/calendar/calendar.component';
 
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
@@ -25,7 +25,33 @@ import { FlatpickrModule } from 'angularx-flatpickr';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { ForgotpasswordComponent } from '@app/pages/forgotpassword/forgotpassword.component';
 import { ForgotusernameComponent } from '@app/pages/forgotusername/forgotusername.component';
-import { RewardsComponent } from './pages/rewards/rewards.component'
+
+import { SocialLoginModule, AuthServiceConfig  } from 'angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider } from 'angularx-social-login';
+import { DemoComponent } from './pages/demo/demo.component';;
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { RewardsComponent} from './pages/rewards/rewards.component';
+
+
+const config = new AuthServiceConfig([
+    {
+        id: GoogleLoginProvider.PROVIDER_ID,
+        //provider: new GoogleLoginProvider('624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com')
+        provider: new GoogleLoginProvider('563025889923-li0ssh373lio431e4m176binneejcqg1.apps.googleusercontent.com')
+    },
+    {
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider('561602290896109')
+    },
+    {
+        id: LinkedInLoginProvider.PROVIDER_ID,
+        provider: new LinkedInLoginProvider('78iqy5cu2e1fgr')
+    }
+]);
+
+export function provideConfig() {
+    return config;
+}
 
 @NgModule({
     imports: [
@@ -45,21 +71,31 @@ import { RewardsComponent } from './pages/rewards/rewards.component'
         NgbModalModule,
         FlatpickrModule.forRoot(),
         CommonModule,
+        SocialLoginModule,
     ],
     declarations: [
         AppComponent,
-        AlertComponent,
+        AlertRetaskComponent,
         HomeComponent,
         LoginComponent,
         RegisterComponent,
         CalendarComponent,
         ForgotpasswordComponent,
         ForgotusernameComponent,
+        DemoComponent,
+        UserProfileComponent,
         RewardsComponent
     ],
     providers: [
+        {
+            provide: AuthServiceConfig,
+            useFactory: provideConfig
+        }
     ],
     bootstrap: [AppComponent]
 })
 
 export class AppModule { }
+
+//{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+//{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
