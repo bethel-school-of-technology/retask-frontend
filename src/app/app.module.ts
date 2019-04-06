92002b59ed8b9f67436c134d8b413412a40f471f
Kayla <kaydanmann@gmail.com>
2019-04-06 14:04:34 -0700

    background calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,13 @@ import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 
 import { AlertRetaskComponent } from './_components';
-//import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+// import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from '@app/pages/home';
 import { LoginComponent } from '@app/pages/login';
 import { RegisterComponent } from '@app/pages/register';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from "./material.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CalendarComponent } from '@app/pages/calendar/calendar.component';
 
@@ -28,7 +28,7 @@ import { ForgotusernameComponent } from '@app/pages/forgotusername/forgotusernam
 
 import { SocialLoginModule, AuthServiceConfig  } from 'angularx-social-login';
 import { GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider } from 'angularx-social-login';
-import { DemoComponent } from './pages/demo/demo.component';;
+import { DemoComponent } from './pages/demo/demo.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { RewardsComponent} from './pages/rewards/rewards.component';
 import { Rewards2Component } from './pages/rewards2/rewards2.component';
@@ -38,7 +38,7 @@ const config = new AuthServiceConfig([
     {
         id: GoogleLoginProvider.PROVIDER_ID,
         provider: new GoogleLoginProvider('624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com')
-        //provider: new GoogleLoginProvider('563025889923-li0ssh373lio431e4m176binneejcqg1.apps.googleusercontent.com')
+        // provider: new GoogleLoginProvider('563025889923-li0ssh373lio431e4m176binneejcqg1.apps.googleusercontent.com')
     },
     {
         id: FacebookLoginProvider.PROVIDER_ID,
@@ -99,5 +99,5 @@ export function provideConfig() {
 
 export class AppModule { }
 
-//{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-//{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+// { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+// { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
