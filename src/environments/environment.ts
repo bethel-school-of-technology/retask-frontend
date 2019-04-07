000d537ecf6c91c8f67a1d429e737a63418227ed
gojohill <gojohill@gmail.com>
2019-04-07 19:26:42 -0400

    added functionality

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:4000',
-  reTaskUrl: 'http://localhost:8090'
+  reTaskUrl: 'http://localhost:8090',
 };
 
 //reTaskUrl: 'http://localhost:8090',
